55dcbe6e6ebbbaa8d2013e4c8c5e5f36fb1dda12
Anne Edmondson <edmondson.anne@gmail.com>
2018-08-20 02:19:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,7 @@
 def prime?(integer)
-  (2...integer).none? {|i| integer % i == 0 ? return true : return false}
+  (2...integer).none? |i|
+  
+  integer % i == 0 ? return true : return false}
   
 end
 
